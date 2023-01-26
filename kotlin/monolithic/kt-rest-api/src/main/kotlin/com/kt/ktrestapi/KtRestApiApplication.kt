838b75aa3177a5d4dcafa7f165c087e1137adfaa
Adrian <adrian.talonia11@gmail.com>
2023-01-25 22:21:07 -0600

    init kotlin api

---
+++ kotlin/monolithic/kt-rest-api/src/main/kotlin/com/kt/ktrestapi/KtRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.kt.ktrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtRestApiApplication>(*args)
+}
